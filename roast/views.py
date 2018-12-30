35fe4ece6bf490295c4eb88aea620796d45ac34c
Guillaume Bédard <bgui34@hotmail.com>
2018-12-30 03:21:57 -0500

    Initial commit

---
+++ roast/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+from django.http import HttpResponse
+
+
+def index(request):
+
+
+
+
+    response_arma= requests.get('https://steamcharts.com/app/107410')
+    response_forhonor = requests.get('https://steamcharts.com/app/304390')
+
+
+    soup = BeautifulSoup(response_arma.text, "html.parser")
+    numbers_arma = soup.find('span', class_='num').get_text()
+
+
+    soup = BeautifulSoup(response_forhonor.text, 'html.parser')
+    numbers_forhonor = soup.find('span', class_='num').get_text()
+
+    content = {'numbers_forhonor' : numbers_forhonor, 'numbers_arma' : numbers_arma}
+    return render(request, 'roast\index.html', content)
